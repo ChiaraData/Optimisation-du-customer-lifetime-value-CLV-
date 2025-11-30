f0b771998d4a5a414a2c68d7ee81292539448d1b
ChiaraData <chiara84700@gmail.com>
2025-11-30 18:56:30 +0100

    add

---
+++ scripts/load_data.py
@@ -0,0 +1,31 @@
+import pandas as pd 
+from utils.config import create_db_engine 
+from sqlalchemy import create_engine
+
+df = pd.read_excel('data/Online Retail.xlsx')
+
+df['Total_Price'] = df['Quantity'] * df['UnitPrice']
+
+print(f"Lignes avant nettoyage: {len(df)}")
+df.dropna(subset=['CustomerID'], inplace=True)
+print(f"Lignes après suppression des NaN CustomerID: {len(df)}")
+
+df = df[(df['Quantity'] > 0) & (df['UnitPrice'] > 0)]
+print(f"Lignes après exclusion des retours/erreurs: {len(df)}")
+
+df['CustomerID'] = df['CustomerID'].astype(str)
+
+
+try :
+    engine = create_engine("mysql+mysqlconnector://root:chiaramasi@localhost/CLV_project")
+    df.to_sql(
+            name='transactions',   
+            con=engine,              
+            if_exists='replace',     
+            index=False,
+            chunksize=10000              
+        )
+    print("les données ont été transféré avec succès dans la base de donnée SQL")
+
+except : 
+    print("les données n'ont pas été transférés dans la base de données.")
---
+++ utils/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+# Charge le fichier .env
+load_dotenv()  
+
+# Récupération des variables
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+print("DB_USER:", DB_USER)  # DEBUG – à enlever ensuite
+
+def create_db_engine():
+    url = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    return create_engine(url)
